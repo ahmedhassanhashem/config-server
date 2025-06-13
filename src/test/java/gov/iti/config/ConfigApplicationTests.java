4108024c2097932892b93e374d4f2c2ee6cc0c0b
AHMED <ahmedhassanhashem@outlook.com>
2025-06-13 17:29:09 +0300

    config-server

---
+++ src/test/java/gov/iti/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.iti.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
